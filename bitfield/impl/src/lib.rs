80492a35f1bb9cf5e9a7b825d08c36edd5e956b8
Jordan McMichael <jlmcmchl@gmail.com>
2021-08-23 21:13:03 -0400

    part 3!

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -2,21 +2,24 @@
 #![feature(proc_macro_diagnostic)]
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, parse_macro_input};
+use syn::{parse_macro_input, DeriveInput};
 
 fn get_offset(target: &syn::Field, fields: &syn::Fields) -> proc_macro2::TokenStream {
     let base = vec![quote! {
         0
     }];
 
-    let calculated_offset = fields.iter().take_while(|f| {
-        (*f).ne(&target)
-    }).map(|field| {
-        let ty = field.ty.clone();
-        quote!{
-            <#ty as bitfield::Specifier>::BITS 
-        }
-    }).chain(base).collect::<Vec<_>>();
+    let calculated_offset = fields
+        .iter()
+        .take_while(|f| (*f).ne(&target))
+        .map(|field| {
+            let ty = field.ty.clone();
+            quote! {
+                <#ty as bitfield::Specifier>::BITS
+            }
+        })
+        .chain(base)
+        .collect::<Vec<_>>();
 
     quote! (
         #(#calculated_offset)+*
@@ -30,11 +33,7 @@ pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
     // eprintln!("INPUT: {:?}", input);
     let ast = parse_macro_input!(input as DeriveInput);
 
-    let fields = if let syn::Data::Struct(syn::DataStruct {
-        fields: named,
-        ..
-    }) = ast.data
-    {
+    let fields = if let syn::Data::Struct(syn::DataStruct { fields: named, .. }) = ast.data {
         named
     } else {
         unimplemented!();
@@ -45,43 +44,74 @@ pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
     let ident = ast.ident;
     let generics = ast.generics;
 
-    let size = fields.iter().map(|field| {
-        let ty = field.ty.clone();
-        quote!{
-            <#ty as bitfield::Specifier>::BITS 
-        }
-    }).collect::<Vec<_>>();
+    let size = fields
+        .iter()
+        .map(|field| {
+            let ty = field.ty.clone();
+            quote! {
+                <#ty as bitfield::Specifier>::BITS
+            }
+        })
+        .collect::<Vec<_>>();
 
-    let accessor_methods = fields.iter().map(|field| {
+    let methods = fields.iter().map(|field| {
         let ident = field.ident.as_ref().unwrap();
-        let fname = syn::Ident::new(&format!("get_{}", ident), proc_macro2::Span::call_site());
+        let get_fname = syn::Ident::new(&format!("get_{}", ident), proc_macro2::Span::call_site());
+        let set_fname = syn::Ident::new(&format!("set_{}", ident), proc_macro2::Span::call_site());
         let ty = field.ty.clone();
         let offset = get_offset(&field, &fields);
 
         quote! {
-            pub fn #fname(&self) -> u64 {
-                let mask = 2_u64.overflowing_pow(<#ty as bitfield::Specifier>::BITS as u32).0.wrapping_sub(1) << (#offset);
-                println!("{:#b}", mask);
-                0
+            pub fn #get_fname(&self) -> u64 {
+                let offset = #offset;
+                let len = <#ty as bitfield::Specifier>::BITS;
+                let mut res: u64 = 0;
+
+                let mut mask = 2_u64.overflowing_pow(<#ty as bitfield::Specifier>::BITS as u32).0.wrapping_sub(1) << offset;
+
+                let mut ind = 0;
+                while mask > 0 {
+                    let temp = self.data[ind] & ((mask & 0xff) as u8);
+                    res <<= 8;
+
+                    res |= (temp as u64);
+                    mask >>= 8;
+
+                    ind += 1;
+                }
+                // println!("READ {:#b}", res);
+                res >> offset
+            }
+
+            pub fn #set_fname(&mut self, arg: u64) {
+                let offset = #offset;
+                let len = <#ty as bitfield::Specifier>::BITS;
+                let mut arg = arg << offset;
+
+                let mut mask = 2_u64.overflowing_pow(<#ty as bitfield::Specifier>::BITS as u32).0.wrapping_sub(1) << offset;
+
+                let mut ind = 0;
+                while arg > 0 {
+                    // println!("WRIT PRE {:?} {:#b}", self.data, arg);
+
+                    // first - clear any bits set in mask
+                    self.data[ind] &= !((mask & 0xff) as u8);
+                    // println!("WRIT CLR {:?}", self.data);
+
+                    // second - set bits according to mask
+                    self.data[ind] |= (arg & mask & 0xff) as u8;
+
+                    // println!("WRIT SET {:?}", self.data);
+
+                    mask >>= 8;
+                    arg >>= 8;
+
+                    ind += 1;
+                }
+                // println!("WRIT END {:?}", self.data);
             }
         }
     }).collect::<Vec<_>>();
-
-    let settor_methods = fields.iter().map(|field| {
-        let ident = field.ident.as_ref().unwrap();
-        let fname = syn::Ident::new(&format!("set_{}", ident), proc_macro2::Span::call_site());
-        let ty = field.ty.clone();
-        let offset = get_offset(&field, &fields);
-
-        quote! {
-            pub fn #fname(&mut self, arg: u64) {
-                let mask = 2_u64.overflowing_pow(<#ty as bitfield::Specifier>::BITS as u32).0.wrapping_sub(1) << (#offset);
-                println!("{:#b}", mask);
-                
-            }
-        }
-    }).collect::<Vec<_>>();
-
 
     let expanded = quote! {
         #[repr(C)]
@@ -96,9 +126,7 @@ pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
                 std::default::Default::default()
             }
 
-            #(#accessor_methods)*
-
-            #(#settor_methods)*
+            #(#methods)*
         }
     };
 
